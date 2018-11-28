531125fe4f4fc21893e9d55a65ca3987526654a9
master <master@oss.vc>
2018-11-28 18:58:43 +0800

    删除部分调试信息

--- Library/Game/Utility/Cry.Game.Utility.cpp
+++ Library/Game/Utility/Cry.Game.Utility.cpp
@@ -16,6 +16,7 @@ namespace Cry
 			{
 
 			}
+			/* INADDR_ANY */
 			bool Utility::OnSocketData(const std::shared_ptr<Cry::Signal::Work> & pWork, const uint32_t uMsg, const void * pData, const uint32_t uSize)
 			{
 				SocketData::CmdRequest Req;
@@ -26,9 +27,7 @@ namespace Cry
 				}
 				Resp.clear_status();
 				Resp.set_status(false);
-				DebugMsg("服务消息 %s\n", Req.content().c_str());
 				return pWork->Send(0, Resp);
-				//INADDR_ANY
 			}
 		};
 	};
